1539da78c80881bab9f095d4ab15505003262e36
udaykumar000 <udaybingundi@gmail.com>
2025-07-23 14:35:57 +0530

    adding get route

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -3,5 +3,8 @@ import upload from "../middleware/multer.middleware.js";
 const router = express.Router();
 
 import { generateReportController } from "../controller/generateReportController.js";
+router.get("/", (req, res) => {
+  return res.status(500).send({ success: true, message: "HEY HOW ARE YOU" });
+});
 router.post("/generate", upload.single("file"), generateReportController);
 export default router;
